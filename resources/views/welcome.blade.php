f3f46418075350b360814d5bf584c40ea040c201
Noel Escobedo <noel_ska15@hotmail.com>
2015-07-18 01:37:04 -0500

    Agregada vista temporal del sitio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,20 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
-
-        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
-    </body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>¡Aqui te vendo!</title>
+    <style>
+    body{
+        font-family: sans-serif, arial;
+    }
+    </style>
+</head>
+<body>
+    <div style="text-align:center">
+        <h1>Aquitevendo.com.mx</h1>
+        <h3>Tenemos a todos nuestros monos trabajando</h3>
+        <img src="{{asset('assets/images/monkey.png')}}" alt="">
+        <h5>"Una nueva experiencia en ventas"</h5>
+    </div>
+</body>
 </html>
