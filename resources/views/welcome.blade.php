4ebbc14fb81069145f1df16b6a715abf5ef298f8
Noel Escobedo <noel_ska15@hotmail.com>
2015-07-22 00:12:50 -0500

    Agregadas fuentes Agregado font-awesome Implementado Materialize Creada hoja de estilos propia del proyecto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,49 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <title>¡Aqui te vendo!</title>
-    <style>
-    body{
-        font-family: sans-serif, arial;
-    }
-    </style>
+    <link rel="stylesheet" href="{{asset('assets/stylesheets/style.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/stylesheets/materialize.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/stylesheets/font-awesome.css')}}">
 </head>
 <body>
+    <header>
+        <div class="container">
+            <a href="#" class="brand">
+                <img src="{{asset('assets/images/monkey.png')}}" alt="" class="logo">
+                <span>Aquitevendo.com.mx</span>
+            </a>
+        </div>
+    </header>
+    <div class="background-one">
+        <div class="container center-align">
+            <br>
+            <h2 class="white-text">¡Tenemos lo que buscas!</h2>
+            <div id="busquedas">
+                <div class="row">
+                    <div class="filter">
+                        <i class="fa fa-car"></i>
+                        <input type="text" placeholder="Filtro 1">
+                    </div>
+                    <div class="filter">
+                        <i class="fa fa-wrench"></i>
+                        <input type="text" placeholder="Filtro 2">
+                    </div>
+                    <div class="button">
+                        <button class="btn waves-effect waves-light" type="submit" name="action">
+                            BUSCAR <i class="fa fa-search"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div style="text-align:center">
         <h1>Aquitevendo.com.mx</h1>
         <h3>Tenemos a todos nuestros monos trabajando</h3>
-        <img src="{{asset('assets/images/monkey.png')}}" alt="">
         <h5>"Una nueva experiencia en ventas"</h5>
     </div>
+    <script type="text/javascript" src="{{asset('assets/stylesheets/materialize.js')}}"></script>
 </body>
 </html>
